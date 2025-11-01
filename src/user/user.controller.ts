287d3988234777d556a9ce1d32792d249a9d59fb
MohamedMansour404 <mohamedmansour.personal1@gmail.com>
2025-11-02 00:01:03 +0200

    .

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { AuthRolesGuard } from './guard/auth-roles.guard';
+import { Roles } from './decorators/user-role.decorator';
+import { UserType } from 'src/utils/enums';
+import * as bcrypt from 'bcrypt';
 
 @ApiTags('users')
 @Controller('user')
@@ -9,6 +13,8 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @UseGuards(AuthRolesGuard)
+  @Roles(UserType.ADMIN)
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
